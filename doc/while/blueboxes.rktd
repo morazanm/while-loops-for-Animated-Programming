e0a8cd3136842c1112d8fbf20950cdefe0e49c91
morazanm <morazanm@gmail.com>
2022-01-07 18:42:59 -0500

    Updated macro with error messages

---
+++ doc/while/blueboxes.rktd
@@ -0,0 +1,6 @@
+84
+((3) 0 () 0 () () (h ! (equal) ((q def ((lib "while/main.rkt") while)) q (0 . 4))))
+procedure
+(while test body) -> (void)
+  test : expression
+  body : (listof expression)
--- while.rkt
+++ while.rkt
@@ -1,23 +1,38 @@
+#lang racket
+
+(require (for-syntax syntax/parse))
+
+;(provide while)
+
+
 ;   Syntax for Animated Programming
 ;   Purpose: Provide while loop construct that returns (void)
 ;
 ;   (while <expr>
-;     <expr>^*)
+;     <expr>^+)
 
+;(define-syntax while
+;  (syntax-rules ()
+;    ((while condition body ...)
+;     (let loop ()
+;       (if condition
+;           (begin
+;             body 
+;             ...
+;             (loop))
+;           (void))))))
 
-
-(module while racket
-
-  (provide while)
-
-  (define-syntax while
-    (syntax-rules ()
-      ((while condition body ...)
-       (let loop ()
+(define-syntax (while stx)
+  (syntax-parse stx
+    [(_ condition body ...+)
+     #`(let loop ()
          (if condition
              (begin
-               body 
-               ...
+               body ...
                (loop))
-             (void)))))))
+             (void)))]
+    [(_ condition)
+     (error (format "Invalid while syntax: Missing body.\n Expected syntax: (while <expr> <expr>^+).\n Given: (while ~s)" (syntax->datum #'condition)))]
+    [else
+     (error (format "Invalid while syntax: Missing driver and body.\n Expected syntax: (while <expr> <expr>^+).\n Given: (while)"))]))
 
